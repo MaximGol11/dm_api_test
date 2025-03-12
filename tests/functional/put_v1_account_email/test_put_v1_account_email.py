15169985581dda9a7d2edf49a634154048474e39
Maxim go <maxim.golubtsov@yahoo.com>
2025-03-12 14:20:15 +0600

    add facade pattern

--- tests/functional/put_v1_account_email/test_put_v1_account_email.py
+++ tests/functional/put_v1_account_email/test_put_v1_account_email.py
@@ -1,11 +1,10 @@
 from faker import Faker
-from api_mailhog.apis.mailhog_api import MailhogApi
-from dm_api_account.apis.account_api import AccountApi
-from dm_api_account.apis.login_api import LoginApi
 from json import loads
 import structlog
 from restclient.configuration import Configuration as MailhogConf
 from restclient.configuration import Configuration as DmApiConf
+from services.serv_api_mailhog import MailHogApi
+from services.serv_dm_api_account import DMApiAccount
 
 
 structlog.configure(
@@ -33,9 +32,8 @@ def get_activation_token_by_login(login, response):
 
 
 def test_put_v1_accounts_email():
-    account_api = AccountApi(dm_api_conf)
-    mailhog_api = MailhogApi(mailhog_conf)
-    login_api = LoginApi(dm_api_conf)
+    account = DMApiAccount(dm_api_conf)
+    mailhog = MailHogApi(mailhog_conf)
 
     login = Faker().user_name()
     password = Faker().password()
@@ -53,16 +51,16 @@ def test_put_v1_accounts_email():
         "password": password
     }
 
-    response = account_api.post_v1_account(json_data=json_data)
+    response = account.account_api.post_v1_account(json_data=json_data)
     assert response.status_code == 201
 
-    response = mailhog_api.get_api_v2_messages()
+    response = mailhog.mailhog_api.get_api_v2_messages()
     assert response.status_code == 200
     token = get_activation_token_by_login(login, response)
-    response = account_api.put_v1_account_token(token=token)
+    response = account.account_api.put_v1_account_token(token=token)
     assert response.status_code == 200
 
-    response = login_api.post_v1_login(json_data=login_data)
+    response = account.login_api.post_v1_login(json_data=login_data)
     assert response.status_code == 200
 
     new_email_data = {
@@ -71,18 +69,18 @@ def test_put_v1_accounts_email():
         "password": password
     }
 
-    response = account_api.put_v1_account_email(json_data=new_email_data)
+    response = account.account_api.put_v1_account_email(json_data=new_email_data)
     assert response.status_code == 200
 
-    response = login_api.post_v1_login(json_data=login_data)
+    response = account.login_api.post_v1_login(json_data=login_data)
     assert response.status_code == 403
 
-    response = mailhog_api.get_api_v2_messages()
+    response = mailhog.mailhog_api.get_api_v2_messages()
     assert response.status_code == 200
     new_token = get_activation_token_by_login(login, response)
 
-    response = account_api.put_v1_account_token(token=new_token)
+    response = account.account_api.put_v1_account_token(token=new_token)
     assert response.status_code == 200
 
-    response = login_api.post_v1_login(json_data=login_data)
+    response = account.login_api.post_v1_login(json_data=login_data)
     assert response.status_code == 200
