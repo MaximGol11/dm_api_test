749df43c5dc9a1edbe4a10dda4c4632f7fbea044
Maxim go <maxim.golubtsov@yahoo.com>
2025-03-20 13:34:15 +0600

    fix

--- dm_api_account/models/change_password.py
+++ dm_api_account/models/change_password.py
@@ -5,5 +5,5 @@ class ChangePassword(BaseModel):
     model_config = ConfigDict(extra="forbid")
     login: str = Field(...)
     token: str = Field(...)
-    old_password: str = Field(..., serealization_alias="oldPassword")
+    old_password: str = Field(..., serialization_alias="oldPassword")
     new_password: str = Field(..., serialization_alias="newPassword")
