60493af222bd586b33406f20e795730b337cb188
Maxim go <maxim.golubtsov@yahoo.com>
2025-03-20 13:28:46 +0600

    add pyhamcreast

--- tests/functional/post_v1_account/test_post_v1_accounts.py
+++ tests/functional/post_v1_account/test_post_v1_accounts.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from hamcrest import assert_that, has_property, has_properties, all_of, starts_with, instance_of, equal_to
 
 
 def test_post_v1_accounts(account_helper, prepare_user_faker):
@@ -6,4 +8,21 @@ def test_post_v1_accounts(account_helper, prepare_user_faker):
     email = prepare_user_faker.email
 
     account_helper.register_and_activate_user(login=login, password=password, email=email)
-    account_helper.user_login(login=login, password=password)
+    response = account_helper.user_login(login=login, password=password)
+    assert_that(
+        response, all_of(
+            has_property("resource", has_property("login", login)),
+            has_property("resource", has_property("registration", instance_of(datetime))),
+            has_property("resource", has_properties(
+                {
+                    "rating": has_properties(
+                        {
+                            "enabled": equal_to(True),
+                            "quality": equal_to(0),
+                            "quantity": equal_to(0)
+                        }
+                    )
+                }
+            ))
+        )
+    )
